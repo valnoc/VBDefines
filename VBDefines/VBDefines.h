ee40bfbbd685482c910dda360f50b768f7c64559
Valeriy Bezuglyy <valnocorner@gmail.com>
2015-01-08 17:52:39 +0300

    update

--- VBDefines/VBDefines.h
+++ VBDefines/VBDefines.h
@@ -26,7 +26,12 @@
 #define VBDefines_h
 
 #ifdef DEBUG
-#define VBLog(str, ...) NSLog(@"[%@ %@]%d> %@", NSStringFromClass([self class]), NSStringFromSelector(_cmd), __LINE__, [NSString stringWithFormat:(str), ##__VA_ARGS__])
+#define VBLog(str, ...) { \
+NSString* func = [NSString stringWithUTF8String:__func__]; \
+NSString* cmdOwner = [[[func componentsSeparatedByString:@" "] firstObject] substringFromIndex:2]; \
+NSString* cmdSelf = NSStringFromClass([self class]); \
+NSLog(@"[%@%@ %@]%d> %@", [cmdSelf isEqualToString:cmdOwner] ? @"" : [NSString stringWithFormat:@"%@:", cmdSelf], cmdOwner, NSStringFromSelector(_cmd), __LINE__, [NSString stringWithFormat:(str), ##__VA_ARGS__]); \
+}
 #else
 #define VBLog(str, ...)
 #endif
