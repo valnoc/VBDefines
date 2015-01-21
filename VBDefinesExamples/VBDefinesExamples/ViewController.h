f3b4ceb9623d77640c1a31fe062d98083f1c23f7
Valeriy Bezuglyy <valnocorner@gmail.com>
2015-01-22 00:57:45 +0300

    added examples project

---
+++ VBDefinesExamples/VBDefinesExamples/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VBDefinesExamples
+//
+//  Created by Valeriy Bezuglyy on 22/01/15.
+//  Copyright (c) 2015 Valeriy Bezuglyy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ VBDefinesExamples/VBDefinesExamples/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  VBDefinesExamples
+//
+//  Created by Valeriy Bezuglyy on 22/01/15.
+//  Copyright (c) 2015 Valeriy Bezuglyy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
